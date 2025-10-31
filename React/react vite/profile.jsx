05e92692f431ea4ca1caa73e26b96eae494a32f3
aboutitsankur <about.itsankur@gmail.com>
2025-10-31 12:25:52 +0530

    React file added

--- React/react vite/profile.jsx
+++ React/react vite/profile.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function Profile(){
+function Profile(props){
     return(
         <div className='profile-container'>
             <h1>User Profile</h1>
             <p>This is the profile page.</p>
-            <h2>Name : Ankur Chaubey</h2>
-            <h2>Branch : IT</h2>
-            <h2>Section : C</h2>
-            <h2>College : ABES Engineering College, Ghaziabad</h2>
+            <h2>{props.name}</h2>
+            <h2>Branch : {props.branch}</h2>
+            <h2>Section : {props.section}</h2>
+            <h2>College :{props.college}</h2>
         </div>
     );
 }
