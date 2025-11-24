3c61f9386a789e4c77a0c1b7e996d5acfe1085db
aboutitsankur <about.itsankur@gmail.com>
2025-11-24 13:06:56 +0530

    cart function added

--- React/react vite/ShoppingCart/vite-project/src/component/Dashboard.jsx
+++ React/react vite/ShoppingCart/vite-project/src/component/Dashboard.jsx
@@ -18,9 +18,33 @@ function Dashboard() {
     }
     getData();
   },[])
+
+function addtocart(ele){
+  alert(ele.title);
+}
+
   return (
     <div>
-      {JSON.stringify(data)}
+      {/* {JSON.stringify(data)} */}
+      {
+        data.length == 0?(
+          <h2>Data not found</h2>
+        ):(
+          <div style={{display:'flex', justifyContent:'space-between'}}>
+            {
+              data.map((ele)=>(
+                <div style={{border:'2px solid blue'}}>
+                  <img src={ele.image} height={100} width={100} />
+                  <h1>{ele.title}</h1>
+                  <h3>{ele.description}</h3>
+                  <h3>{ele.price}</h3>
+                  <button onClick={()=>addtocart(ele)}>Add to Cart</button>
+                </div>
+              ))
+            }
+          </div>
+        )
+      }
     </div>
   )
 }
